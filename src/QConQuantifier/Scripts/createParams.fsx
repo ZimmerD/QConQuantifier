790c266aa539688ae88d5a6aeef5a9a478458232
ZimmerD <davidzimmer91@gmail.com>
2019-05-21 01:01:13 +0200

    add qConcat as script add referencing script

---
+++ src/QConQuantifier/Scripts/createParams.fsx
@@ -0,0 +1,39 @@
+#load "references.fsx"
+
+open BioFSharp.Mz
+open SearchDB
+open QConQuantifier
+open Parameters.Domain
+open Parameters.DTO
+
+let standardQConQuantifierParams :Parameters.DTO.QConQuantifierParams = 
+    {
+    Name                            = "ChlamyDB"
+    DbFolder                        = ""
+    QConCatFastaPaths               = [""]
+    OrganismFastaPath               = ""
+    ParseProteinIDRegexPattern      = "id"
+    Protease                        = Protease.Trypsin
+    MinMissedCleavages              = 0
+    MaxMissedCleavages              = 1
+    MaxMass                         = 15000.
+    MinPepLength                    = 5
+    MaxPepLength                    = 50
+    IsotopicMod                     = [IsotopicMod.N15]
+    MassMode                        = MassMode.Monoisotopic
+    FixedMods                       = []            
+    VariableMods                    = []
+    VarModThreshold                 = 3
+    ExpectedMinCharge               = 2
+    ExpectedMaxCharge               = 3
+    LookUpPPM                       = 30.
+    ScanRange                       = 100.0,1600.0
+    PSMThreshold                    = PSMThreshold.PepValue 0.05
+    ScanTimeWindow                  = 2.5
+    MzWindow_Da                     = 0.07
+    NTerminalSeries                 = NTerminalSeries.B
+    CTerminalSeries                 = CTerminalSeries.Y
+    } 
+
+
+System.IO.File.WriteAllText(@"C:\Users\david\Source\Repos\netCoreRepos\QConQuantifier\src\QConQuantifierConsole\params.Json",Newtonsoft.Json.JsonConvert.SerializeObject standardQConQuantifierParams)
