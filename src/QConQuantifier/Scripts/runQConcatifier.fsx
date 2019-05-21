6410adb9ae6724e91b3720651e8f1b62468d6f6f
ZimmerD <davidzimmer91@gmail.com>
2019-05-21 09:29:44 +0200

    uncommented runQConcatifier.fsx not thorougly tested.

--- src/QConQuantifier/Scripts/runQConcatifier.fsx
+++ src/QConQuantifier/Scripts/runQConcatifier.fsx
@@ -1,71 +1,72 @@
-#load "references.fsx"
+// not thorougly tested.
+////#load "references.fsx"
 
-open System
-open System.IO
-open Deedle
-open FSharpAux
-open BioFSharp.Mz
-open QConQuantifier
-open Parameters.Domain
-open Parameters.DTO
+//open System
+//open System.IO
+//open Deedle
+//open FSharpAux
+//open BioFSharp.Mz
+//open QConQuantifier
+//open Parameters.Domain
+//open Parameters.DTO
 
-let sourceD = __SOURCE_DIRECTORY__
+//let sourceD = __SOURCE_DIRECTORY__
 
-let qConQuantifierParams = 
-    {
-    Name                            = "ChlamyTruncDB"
-    DbFolder                        =  @"C:\Users\david\Source\Repos\netCoreRepos\QConcatifier_Samples\db"
-    QConCatFastaPaths               = [@"C:\Users\david\Source\Repos\netCoreRepos\QConcatifier_Samples\fasta\PS QconCAT.fasta"]
-    OrganismFastaPath               =  @"C:\Users\david\Source\Repos\netCoreRepos\QConcatifier_Samples\fasta\Chlamy_JGI5_trunc.fasta"
-    ParseProteinIDRegexPattern      = "id"
-    Protease                        = Protease.Trypsin
-    MinMissedCleavages              = 0
-    MaxMissedCleavages              = 1
-    MaxMass                         = 15000.
-    MinPepLength                    = 5
-    MaxPepLength                    = 50
-    IsotopicMod                     = [IsotopicMod.N15]
-    MassMode                        = MassMode.Monoisotopic
-    FixedMods                       = []            
-    VariableMods                    = []
-    VarModThreshold                 = 3
-    ExpectedMinCharge               = 2
-    ExpectedMaxCharge               = 3
-    LookUpPPM                       = 30.
-    ScanRange                       = 100.0,1600.0
-    PSMThreshold                    = PSMThreshold.PepValue 0.05
-    ScanTimeWindow                  = 2.5
-    MzWindow_Da                     = 0.07
-    NTerminalSeries                 = NTerminalSeries.B
-    CTerminalSeries                 = CTerminalSeries.Y
-    } 
+//let qConQuantifierParams = 
+//    {
+//    Name                            = "ChlamyTruncDB"
+//    DbFolder                        =  @"C:\Users\david\Source\Repos\netCoreRepos\QConcatifier_Samples\db"
+//    QConCatFastaPaths               = [@"C:\Users\david\Source\Repos\netCoreRepos\QConcatifier_Samples\fasta\PS QconCAT.fasta"]
+//    OrganismFastaPath               =  @"C:\Users\david\Source\Repos\netCoreRepos\QConcatifier_Samples\fasta\Chlamy_JGI5_trunc.fasta"
+//    ParseProteinIDRegexPattern      = "id"
+//    Protease                        = Protease.Trypsin
+//    MinMissedCleavages              = 0
+//    MaxMissedCleavages              = 1
+//    MaxMass                         = 15000.
+//    MinPepLength                    = 5
+//    MaxPepLength                    = 50
+//    IsotopicMod                     = [IsotopicMod.N15]
+//    MassMode                        = MassMode.Monoisotopic
+//    FixedMods                       = []            
+//    VariableMods                    = []
+//    VarModThreshold                 = 3
+//    ExpectedMinCharge               = 2
+//    ExpectedMaxCharge               = 3
+//    LookUpPPM                       = 30.
+//    ScanRange                       = 100.0,1600.0
+//    PSMThreshold                    = PSMThreshold.PepValue 0.05
+//    ScanTimeWindow                  = 2.5
+//    MzWindow_Da                     = 0.07
+//    NTerminalSeries                 = NTerminalSeries.B
+//    CTerminalSeries                 = CTerminalSeries.Y
+//    } 
 
-System.IO.File.WriteAllText(Path.Combine [|sourceD;"sampleParams.Json"|],Newtonsoft.Json.JsonConvert.SerializeObject qConQuantifierParams)
+//System.IO.File.WriteAllText(Path.Combine [|sourceD;"sampleParams.Json"|],Newtonsoft.Json.JsonConvert.SerializeObject qConQuantifierParams)
     
 
-let qparams = qConQuantifierParams  |> QConQuantifierParams.toDomain
-let inputDirectory  = @"C:\Users\david\Source\Repos\netCoreRepos\QConcatifier_Samples"
-let outputDirectory = @"C:\Users\david\Source\Repos\netCoreRepos\QConcatifier_Samples\out"
-let numberOfCores = 4
+//let qparams = qConQuantifierParams  |> QConQuantifierParams.toDomain
+//let inputDirectory  = @"C:\Users\david\Source\Repos\netCoreRepos\QConcatifier_Samples"
+//let outputDirectory = @"C:\Users\david\Source\Repos\netCoreRepos\QConcatifier_Samples\out"
+//let numberOfCores = 4
 
-///
-let inputFiles = DirectoryInfo(inputDirectory).GetFiles("*.mzlite")
-///            
-let peptideDB = PeptideLookUp.dbLookUpCn qparams
-///
-let res = 
-    inputFiles 
-    |> PSeq.map (fun f -> 
-                    printfn "Start analyzing: %s" f.FullName
-                    let res = Pipeline.analyzeFile peptideDB qparams outputDirectory f.FullName
-                    printfn "Finished analyzing: %s" f.FullName
-                    res
-                )
-    |> PSeq.withDegreeOfParallelism numberOfCores
-    |> List.ofSeq
-    |> Pipeline.mergeFrames
+/////
+//let inputFiles = DirectoryInfo(inputDirectory).GetFiles("*.mzlite")
+/////            
+//let peptideDB = PeptideLookUp.dbLookUpCn qparams
+/////
+//let res = 
+//    inputFiles 
+//    |> PSeq.map (fun f -> 
+//                    printfn "Start analyzing: %s" f.FullName
+//                    let res = Pipeline.analyzeFile peptideDB qparams outputDirectory f.FullName
+//                    printfn "Finished analyzing: %s" f.FullName
+//                    res
+//                )
+//    |> PSeq.withDegreeOfParallelism numberOfCores
+//    |> List.ofSeq
+//    |> Pipeline.mergeFrames
 
-///
-let outFilePath = Path.Combine [|outputDirectory;"QuantifiedPeptides.txt"|]
-res.SaveCsv(outFilePath,includeRowKeys=true,separator='\t',keyNames=["StringSequence";"GlobalMod";"Charge"])
+/////
+//let outFilePath = Path.Combine [|outputDirectory;"QuantifiedPeptides.txt"|]
+//res.SaveCsv(outFilePath,includeRowKeys=true,separator='\t',keyNames=["StringSequence";"GlobalMod";"Charge"])
 
