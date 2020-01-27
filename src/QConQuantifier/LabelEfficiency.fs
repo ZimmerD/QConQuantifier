2334fbbbf309d5c9e2649ddf8be2488dcf101b90
kMutagene <kevin-schneider@mutagene.de>
2020-01-27 10:14:17 +0100

    Add comments

--- src/QConQuantifier/LabelEfficiency.fs
+++ src/QConQuantifier/LabelEfficiency.fs
@@ -43,11 +43,13 @@ module LabelEfficiency =
             CorrectionFactor        : float
         }
 
+    ///C0nverts a float tuple list to its full String representation. Sadly needed as Deedle does use ToString() for patterns otherwise
     let stringOfPattern (p: (float*float) list) =
         p
         |> List.fold (fun acc (mz,prob) -> if acc = "" then (sprintf "(%f,%f)" mz prob) else sprintf "%s;(%f,%f)" acc mz prob) ""
         |> fun x -> sprintf "[%s]" x
-
+    
+    ///C0nverts a float list tuple String representation to the list type. Sadly needed as Deedle does use ToString() for patterns otherwise
     let patternOfString (patternString:string) =
         if patternString = "[]" then 
             []
@@ -90,16 +92,14 @@ module LabelEfficiency =
             CorrectionFactor            = correctionFactor 
         }
 
+    ///returns a function that replaces the nitrogen atoms in a formula with a nitrogen with the given probability of being the 15N isotope
     let initlabelN15Partial n15Prob =
         ///Diisotopic representation of nitrogen with abundancy of N14 and N15 swapped
         let n14Prob = 1. - n15Prob
         let N15 = Di (createDi "N15" (Isotopes.Table.N15,n15Prob) (Isotopes.Table.N14,n14Prob) )
         fun f -> Formula.replaceElement f Elements.Table.N N15
 
-    let labelFullN15 =
-        let N15 = Elements.Table.Heavy.N15
-        fun f -> Formula.replaceElement f Elements.Table.N N15
-
+    ///Predicts an isotopic distribution of the given formula at the given charge, normalized by the sum of probabilities, using the MIDAs algorithm
     let generateIsotopicDistributionOfFormulaBySum (charge:int) (f:Formula.Formula) =
         IsotopicDistribution.MIDA.ofFormula 
             IsotopicDistribution.MIDA.normalizeByProbSum
@@ -108,6 +108,7 @@ module LabelEfficiency =
             charge
             f
 
+    ///Predicts an isotopic distribution of the given formula at the given charge, normalized by the maximum probability, using the MIDAs algorithm
     let generateIsotopicDistributionOfFormulaByMax (charge:int) (f:Formula.Formula) =
         IsotopicDistribution.MIDA.ofFormula 
             IsotopicDistribution.MIDA.normalizeByMaxProb
@@ -116,8 +117,10 @@ module LabelEfficiency =
             charge
             f
 
+    ///Predicts the Label efficiency of the given Peptide by finding the best fitting Isotopic distribution amongst distributions generated at various label efficiencies.
     let predictLabelEfficiency (qP : QuantifiedPeptide) =
 
+        //no need to predict anything if one of the peaks is missing.
         if (nan.Equals(qP.N15Minus1Quant)) || (nan.Equals(qP.N15Quant)) then
 
             createLabelEfficiencyPredictor
@@ -135,6 +138,7 @@ module LabelEfficiency =
 
             let peakRatio = qP.N15Minus1Quant / qP.N15Quant
 
+            //Generate "testing" distributions (patterns at various label efficiencies)
             let theoreticalIsotopicDistributions =
                 [for i in 0.5 .. 0.001 .. 0.999 do
                     //if ((int (i*1000.)) % 100) = 0 then
@@ -147,7 +151,7 @@ module LabelEfficiency =
                         |> generateIsotopicDistributionOfFormulaByMax qP.Charge
                 ]
 
-
+            //for each predicted pattern, couple it with label efficiency and n15 / n15-1 ratio
             let theoreticalRatios =
                 theoreticalIsotopicDistributions
                 |> List.map 
@@ -169,7 +173,8 @@ module LabelEfficiency =
                             |> snd
                         le,(n15Minus1Prob / n15Prob), dist
                     )
-
+            
+            //find best fitting pattern by minimizing the absolute difference between the N15/N15-1 ratios
             let predictedLE, ratio, predictedDist = 
                 theoreticalRatios
                 |> List.minBy
@@ -185,6 +190,7 @@ module LabelEfficiency =
                 predictedLE
                 [(qP.N15Minus1MZ,qP.N15Minus1Quant);(qP.N15MZ,qP.N15Quant)]
 
+    ///Calculates median label efficiency after filtering the label efficiency distribution using the tukey interquartile range criterion, initialized with C=3
     let getFilteredMedianLabelEfficiency plotDirectory tukeyC (predictors: LabelEfficiencyPredictor []) =
 
         let lEDist =
@@ -203,11 +209,15 @@ module LabelEfficiency =
         filtered
         |> Seq.median 
 
+    ///For all given LabelEfficiency predictors, adds the correction factor needed to correct the N15 value as if the label efficiency would be 100%. 
+    ///This is calculated by predicting the isotopic patterns at median label (the proxy for the label efficiency of the qconcat protein) efficiency and at 100% label efficiency. The correction Factor is the 
+    ///is the probability peak of 15N at median LE / probability peak of N15 at 100%Le
     let estimateCorrectionFactors (filteredMedianPredictedLabelEfficiency:float) (predictors: LabelEfficiencyPredictor []) : LabelEfficiencyResult [] =
         
         predictors
         |> Array.map 
             (fun lePredictor ->
+                //if we cannot predict LE, return an uncorrected result.
                 if nan.Equals(lePredictor.PredictedLabelEfficiency) then
                     createLabelEfficiencyResult
                         lePredictor.StringSequence
